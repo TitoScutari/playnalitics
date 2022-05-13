c071a3990052726712f2f6912517cc1883836e4c
tito scutari <titoscutari@gmail.com>
2022-05-13 11:30:38 +0200

    db.sql completed

--- db.sql
+++ db.sql
@@ -1,29 +1,31 @@
 DROP DATABASE IF EXISTS playnaliticsDB;
-CREATE DATABASE IF NOT EXISTS playnaliticsDB;
+CREATE DATABASE playnaliticsDB;
 USE playnaliticsDB;
 
 CREATE TABLE playlists (
-    Id VARCHAR() NOT NULL PRIMARY KEY
-    userId VARCHAR() NOT NULL
-    title VARCHAR(100) NOT NULL
+    Id VARCHAR(22) NOT NULL PRIMARY KEY,
+    userId VARCHAR(10) NOT NULL,
+    title VARCHAR(100) NOT NULL,
     loaded BOOLEAN NOT NULL
 );
 
 CREATE TABLE tracks (
-    Id VARCHAR() NOT NULL PRIMARY KEY
-    title VARCHAR(100) NOT NULL
-    artist VARCHAR(50) NOT NULL
-    acusticness FLOAT(10) NOT NULL
-    danceability FLOAT(10) NOT NULL
-    energy FLOAT(10) NOT NULL
-    valence FLOAT(10) NOT NULL
-    loudness FLOAT(10) NOT NULL
+    Id VARCHAR(22) NOT NULL PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    artist VARCHAR(50) NOT NULL,
+    acusticness FLOAT(10) NOT NULL,
+    danceability FLOAT(10) NOT NULL,
+    energy FLOAT(10) NOT NULL,
+    valence FLOAT(10) NOT NULL,
+    loudness FLOAT(10) NOT NULL,
+    musicKey VARCHAR(2),
+    bpm INT 
 );
 
 CREATE TABLE playlisttracks(
-    IdTrack VARCHAR() NOT NULL
-    IdPlaylist VARCHAR() NOT NULL
-    FOREIGN KEY (IdTrack) REFERENCES tracks(Id)
-    FOREIGN KEY (IdPlaylist) REFERENCES playlists(Id)
+    IdTrack VARCHAR(22) NOT NULL,
+    IdPlaylist VARCHAR(22) NOT NULL,
+    FOREIGN KEY (IdTrack) REFERENCES tracks(Id),
+    FOREIGN KEY (IdPlaylist) REFERENCES playlists(Id),
     PRIMARY KEY(IdTrack, IdPlaylist)
 )

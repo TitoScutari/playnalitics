daacab86eb3292b1ef5cec04ea5d5ad700c50f30
tito scutari <titoscutari@gmail.com>
2022-05-15 16:36:07 +0200

    spotify_obj.py

---
+++ spotify_obj.py
@@ -0,0 +1,26 @@
+import spotipy
+import credentials
+import pandas as pd
+from abc import ABC, abstractmethod
+
+class SpotifyObj(ABC):
+
+    def __init__(self, id:str):
+        self.__id = id
+        self.__spotify = spotipy.Spotify(
+            auth_manager=spotipy.SpotifyOAuth(
+            client_id=credentials.client_id(), 
+            client_secret=credentials.client_secret(),
+            redirect_uri="http://localhost"))
+
+    @property
+    def id(self):
+        return self.__id
+
+    @abstractmethod
+    def get_data(self):
+        pass
+
+    @abstractmethod
+    def to_dataframe(self):
+        pass
